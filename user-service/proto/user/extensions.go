1bde49c6feff0c1fc65eaf61cdf99a4bd22f8259
Razvan <razvan@fieldmargin.com>
2018-04-10 16:00:50 +0100

    added new service for users

--- user-service/proto/user/extensions.go
+++ user-service/proto/user/extensions.go
@@ -3,9 +3,13 @@ package go_micro_srv_user
 import (
 	"github.com/jinzhu/gorm"
 	"github.com/satori/go.uuid"
+	"log"
 )
 
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	uuid, _ := uuid.NewV4()
+	uuid, error := uuid.NewV4()
+	if error != nil {
+		log.Println(error)
+	}
 	return scope.SetColumn("Id", uuid.String())
 }
